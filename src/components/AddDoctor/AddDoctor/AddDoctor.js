8f9595329cd92401a6d475c735109dc9758aa486
as ahmed <rakibasrr13@gmail.com>
2020-10-14 15:14:24 +0300

    Upload file

--- src/components/AddDoctor/AddDoctor/AddDoctor.js
+++ src/components/AddDoctor/AddDoctor/AddDoctor.js
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../Dashboard/Sidebar/Sidebar';
 
 const AddDoctor = () => {
+
+    // handle name and gmail ------------------------/
+    const [info, setInfo] = useState({});
+    const handleBlur = e => {
+        const newInfo = { ...info };
+        newInfo[e.target.name] = e.target.value;
+        setInfo(newInfo);
+    };
+
+
+    // handle file image -----------------------/
+    const [file, setFile] = useState(null);
+    const handleFileChange = e => {
+        const newFile = e.target.files[0];
+        setFile(newFile);
+    };
+
+
+
+    // handle form Submit----------------/
+    const handleSubmit = () => {
+        const formData = new FormData()
+        formData.append('file', file);
+        formData.append('name', info.name);
+        formData.append('email', info.email);
+
+        fetch('http://localhost:5000/addDoctors', {
+            method: 'POST',
+            body: formData
+        }) 
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+
     return (
         <section className="container-fluid row">
             <Sidebar></Sidebar>
-            <div className="col-md-10 p-4 pr-5">
-                <h5 className="text_brand">Add a Doctor</h5>
-                <form>
+            <div className="col-md-10 p-4 pr-5" style={{ position: "absolute", right: 0, backgroundColor: "#F4FDFB" }}>
+                <form onSubmit={handleSubmit}>
+                    <h5 className="text_brand">Add a Doctor</h5>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <input type="email" class="form-control" aria-describedby="emailHelp" name="email" onBlur={handleBlur} placeholder="Enter your Email" />
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" name="name" onBlur={handleBlur} placeholder="Name" />
                     </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <div class="form-group">
+                        <label for="name">Upload a image</label>
+                        <input onChange={handleFileChange} type="file" class="form-control" placeholder="Picture" />
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
